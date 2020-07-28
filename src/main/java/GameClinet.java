46267bf5c3eb72dab356898db9ceea4272677434
FrankTsai <cheungthe@gmail.com>
2020-07-28 21:36:47 +0800

    add Frame

--- src/main/java/GameClinet.java
+++ src/main/java/GameClinet.java
@@ -1,7 +1,34 @@
-public class GameClinet {
-    public static void main(String[] args) {
-        //TankWar Star
-        System.out.println("1");
+import javax.swing.*;
+import java.awt.*;
 
+public class GameClinet extends JComponent {
+    private int screenWidth;
+    private int screenHeight;
+
+
+    GameClinet(){
+        this(800,600);
+    }
+
+    GameClinet(int screenWidth,int screenHeight){
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+    }
+
+    public int getScreenWidth() {
+        return screenWidth;
+    }
+
+    public void setScreenWidth(int screenWidth) {
+        this.screenWidth = screenWidth;
+    }
+
+    public int getScreenHeight() {
+        return screenHeight;
+    }
+
+    public void setScreenHeight(int screenHeight) {
+        this.screenHeight = screenHeight;
     }
 }
---
+++ src/main/java/TankWarGame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class TankWarGame {
+    public static void main(String[] args) {
+        //TankWar Star Game
+        JFrame frame = new JFrame();
+        frame.add(new GameClinet(1430,825));
+        frame.setTitle("TankWar - V1");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+    }
+}
